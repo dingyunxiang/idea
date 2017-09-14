11b247b4b45a9e70e66bb5f3f9c9d81f91727a2c
云相 <yunxiang.dyx@alibaba-inc.com>
2017-09-14 22:08:59 +0800

    Initial commit

---
+++ src/main/java/cn/dingyunxiang/idea/IdeaApplication.java
@@ -0,0 +1,18 @@
+package cn.dingyunxiang.idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * IDEA启动类
+ *
+ * @author:yunxiang.dyx
+ * @date:2017-09-14
+ */
+@SpringBootApplication
+public class IdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaApplication.class, args);
+    }
+}
